98fe798980686cb235980a491e5d1ca8c6f7f7ed
Tom Milostny <tommilostny@live.com>
2020-04-29 19:46:30 +0200

    Updated load_arg description

--- proj2/proj2.h
+++ proj2/proj2.h
@@ -15,7 +15,7 @@
 #define create_shared_var(type) \
 	mmap(NULL, sizeof(type), PROT_READ|PROT_WRITE, MAP_SHARED|MAP_ANONYMOUS, -1, 0)
 
-//Načte argument programu a vrací jeho celočíselnou hodnotu v nově alokované sdílené paměti
+//Načte argument programu a vrací nově alokovaný integer ve sdílené paměti
 int *load_arg(char **argv, int argv_index);
 
 //Tisk do souboru (jako fprintf), zajištění okamžitého zápisu do souboru pomocí fflush()
